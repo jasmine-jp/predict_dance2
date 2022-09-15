9dc30eadf915ffaa7101f100b31a9f5f29461f52
humanmodeling OIT <humanmodelingoit@humanmodelingnoMacBook-Air-3.local>
2022-09-16 00:21:14 +0900

    add bias each of conv

--- src/py/network.py
+++ src/py/network.py
@@ -8,29 +8,32 @@ class NeuralNetwork(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv2d = nn.Sequential(
+        self.cl = nn.ModuleList([nn.Sequential(
             nn.Conv2d(1, sec_d, sec_size, sec_size),
+            nn.BatchNorm2d(sec_d),
             nn.ReLU(),
             nn.MaxPool2d(pool),
             nn.Conv2d(sec_d, thr_d, thr_size, thr_size),
+            nn.BatchNorm2d(thr_d),
             nn.ReLU(),
             nn.MaxPool2d(pool),
             nn.Flatten()
-        )
+        ) for _ in range(batch)])
 
-        self.el = nn.TransformerEncoderLayer(out,2,batch_first=True)
+        e = nn.TransformerEncoderLayer(out,1,batch_first=True)
+        self.encoder = nn.TransformerEncoder(e, 2)
 
         self.stack = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(out*arr_size, 256),
+            nn.Linear(out*arr_size, 32),
             nn.ReLU(),
-            nn.Linear(256, 256),
+            nn.Linear(32, 8),
             nn.ReLU(),
-            nn.Linear(256, lenA),
+            nn.Linear(8, lenA),
             nn.Softmax(1)
         )
 
     def forward(self, x):
-        self.c = torch.stack(list(map(self.conv2d, x)))
-        self.e = self.el(self.c)
+        self.c = torch.stack(list(map(lambda c,e:c(e),self.cl,x)))
+        self.e = self.encoder(self.c)
         return self.stack(self.e)
--- test.py
+++ test.py
@@ -6,7 +6,11 @@ from src.py.plot import plot
 # print('Input Size:', 10, arr_size, 1, size, size)
 # torchinfo.summary(NeuralNetwork(), (10, arr_size, 1, size, size))
 
+if input('archive [y/n]: ') == 'y':
+    r, i = all_read('archive'), 10000
+else:
+    r, i = test_read(), 750
 model = torch.load('out/model/model_weights.pth')
-study = Study(model, test_read(), 750, plot(False))
+study = Study(model, r, i, plot(False))
 
 study.test()
